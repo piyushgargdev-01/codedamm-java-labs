de8fa791c084d6072c05becf173a047c673c72b6
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-09-22 11:36:08 +0530

    Init

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
 public class Main {
     public static void main(String args[]) {
+        // Integers
         int age = 22;
         int x = 5;
 
+        // Floats
+        float pi = 3.14f;
+
+        // Booleans
+        boolean isGraduated = true;
+
+        // Chars
+        char gender = 'm';
+
         System.out.println("Your age is " + age);
-        System.out.println("Value of x is " + x);
+        System.out.println("The value of x is " + x);
+
+        System.out.println("PI = " + pi);
+
+        System.out.println("Am I graduated? - " + isGraduated);
+
+        System.out.println("My Gender is - " + gender);
+        
     }
 }
