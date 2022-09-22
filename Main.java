be66b79b50066d85c10222cd60b8933b045852c9
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-09-22 14:15:39 +0530

    Remove Code

--- Main.java
+++ Main.java
@@ -1,22 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String args[]) {
-       Scanner scanner = new Scanner(System.in);
-
-       int a, b; // Declare two variables
-
-       System.out.print("Enter value of a: ");
-       a = scanner.nextInt();
-       
-       System.out.print("Enter value of b: ");
-       b = scanner.nextInt();
-
-       System.out.println("Addition: " + (a + b));
-       System.out.println("Subtract: " + (b - a));
-       System.out.println("Multiply: " + (a * b));
-       System.out.println("Divide: " + (b / a));
+      // Write your code here...
       
-        
     }
 }
