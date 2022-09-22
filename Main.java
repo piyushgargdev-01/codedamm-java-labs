7e97d103719fa4763618471aace984bd82d26619
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-09-23 01:21:06 +0530

    Init

--- Main.java
+++ Main.java
@@ -1,36 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 
   public static void main(String args[]) {
-    Scanner scanner = new Scanner(System.in);
-
-    int x, y, result = 0;
-    char ch;
-
-    System.out.print("Enter value for x: ");
-    x = scanner.nextInt();
-
-    System.out.print("Enter value for y: ");
-    y = scanner.nextInt();
-
-    System.out.print("Enter a symbol ( +, -, * ): ");
-    ch = scanner.next().charAt(0);
-
-    switch (ch) {
-      case '+':
-        result = x + y;
-        break;
-      case '-':
-        result = x - y;
-        break;
-      case '*':
-        result = x * y;
-        break;
-      default:
-        System.out.println("Invalid Option");
-    }
-
-    System.out.println("Result: " + result);
+    // Write your code here...
   }
 }
