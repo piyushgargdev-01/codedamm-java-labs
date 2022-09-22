695e622fb311c9d9077b55326815553cd85c8e5c
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-09-22 11:50:00 +0530

    Init

--- Main.java
+++ Main.java
@@ -4,10 +4,19 @@ public class Main {
     public static void main(String args[]) {
        Scanner scanner = new Scanner(System.in);
 
-       System.out.print("Whats your age? ");
-       int age = scanner.nextInt();
+       int a, b; // Declare two variables
 
-       System.out.println("Your age is " + age);
+       System.out.print("Enter value of a: ");
+       a = scanner.nextInt();
+       
+       System.out.print("Enter value of b: ");
+       b = scanner.nextInt();
+
+       System.out.println("Addition: " + (a + b));
+       System.out.println("Subtract: " + (b - a));
+       System.out.println("Multiply: " + (a * b));
+       System.out.println("Divide: " + (b / a));
+      
         
     }
 }
