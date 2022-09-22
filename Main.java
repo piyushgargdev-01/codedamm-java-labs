91b926ff6ab8e718839fd50cb30bda9c0ef66475
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-09-22 11:28:33 +0530

    Init

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        int age = 22;
+        int x = 5;
+
+        System.out.println("Your age is " + age);
+        System.out.println("Value of x is " + x);
     }
 }
