a115297c4844ab7d1654f1a4dd0b2d89ce772583
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-09-24 00:36:42 +0530

    Init

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String args[]) {
-    // Write your code here...
+    Scanner scanner = new Scanner(System.in);
+
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    // Write your logic here to swap a and b
+
+    /* Do not alter the below code */
+    System.out.println(a);
+    System.out.println(b);
   }
 }
