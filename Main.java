7dc383de42b232ce12e27647fcf57ef0a4d80649
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-10-02 11:07:53 +0530

    Init

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String args[]) {
-    // Write your code here...
+    Scanner scanner = new Scanner(System.in);
+
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    try {
+      System.out.println(a / b);
+    } catch (ArithmeticException e) {
+      // TODO: handle exception
+      System.out.println("ArithmeticException " + e);
+    }
   }
 }
