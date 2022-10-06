0d079b2e573e3fbd144d30c81e85f9fe68c8292f
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-10-06 17:10:47 +0530

    Init

--- Main.java
+++ Main.java
@@ -1,16 +1,14 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
-  public static void main(String args[]) {
-    int[] arr = new int[] { 1, 1, 1, 2, 3, 2, 4, 4, 5, 6 };
-    Set<Integer> set = new HashSet<>();
+  public static void main(String[] args) {
+    Map<Integer, String> map = new HashMap<>();
+    map.put(1, "Piyush");
+    map.put(2, "Jhon");
+    map.put(3, "Jane");
 
-    for (int i = 0; i < arr.length; i++) {
-      set.add(arr[i]);
-    }
-
-    System.out.println(set);
+    System.out.println(map.get(1));
   }
 }
