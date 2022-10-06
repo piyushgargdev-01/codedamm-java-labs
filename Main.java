9f0bf5923f4d1b3852118fdb7b52cbe0fe273b14
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-10-06 16:43:02 +0530

    Init

--- Main.java
+++ Main.java
@@ -1,18 +1,14 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
   public static void main(String args[]) {
-    Scanner scanner = new Scanner(System.in);
+    int[] arr = new int[5];
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
 
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
-
-    try {
-      System.out.println(a / b);
-    } catch (ArithmeticException e) {
-      // TODO: handle exception
-      System.out.println("ArithmeticException " + e);
-    }
+    System.out.println(Arrays.toString(arr));
   }
 }
