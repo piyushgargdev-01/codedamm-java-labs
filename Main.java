211ae18dc2da7e03dedef581d74682f4e0444f8a
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-10-06 17:31:13 +0530

    Init

--- Main.java
+++ Main.java
@@ -1,98 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
+class DatabaseService {
 
-class Person {
+  private static DatabaseService obj;
 
-  private String fullName;
-
-  Person(String fullName) {
-    this.fullName = fullName;
+  private DatabaseService() {
+    System.out.println("Open a connection with DB");
   }
 
-  public String getFullName() {
-    return this.fullName;
+  public static DatabaseService getInstance() {
+    if (obj == null) obj = new DatabaseService();
+    return obj;
   }
 
-  public void setFullName(String name) {
-    this.fullName = name;
-  }
-}
-
-class Student extends Person {
-
-  private int rollNumber;
-  private List<Student> friends;
-
-  static int numberOfStudents = 0;
-
-  Student(String fullname, int rollNumber) {
-    super(fullname);
-    this.friends = new ArrayList<>();
-    this.rollNumber = rollNumber;
-    numberOfStudents++;
+  public void getDataFromTables() {
+    System.out.println("Getting data from db");
   }
 
-  public int getRollNumber() {
-    return this.rollNumber;
-  }
-
-  public void setRollNumber(int newRoll) {
-    this.rollNumber = newRoll;
-  }
-
-  public List<Student> getFriends() {
-    return this.friends;
-  }
-
-  public void addFriend(Student student) {
-    this.friends.add(student);
-    student.friends.add(this);
-  }
-
-  public void removeFriend(Student student) {
-    this.friends.remove(student);
-    student.friends.remove(this);
-  }
-
-  public boolean isFriendOf(Student student) {
-    return this.friends.contains(student);
-  }
-
-  public int getRollNo(Student student) throws IllegalAccessException {
-    if (
-      this.friends.contains(student)
-    ) return student.rollNumber; else throw new IllegalAccessException(
-      "You are not friend of " + student.getFullName()
-    );
-  }
-
-  public void removeAllFriends() {
-    for (Student friend : this.friends) {
-      friend.friends.remove(this);
-      this.friends.remove(friend);
-    }
-  }
-
-  public String toString() {
-    return this.getFullName();
+  public void putDataInDatabase() {
+    System.out.println("Putting data in db");
   }
 }
 
 public class Main {
 
   public static void main(String args[]) {
-    Student piyush = new Student("Piyush", 1);
-    Student jhon = new Student("Jhon", 2);
-    Student jane = new Student("Jane", 3);
-    Student parry = new Student("Parry", 4);
+    DatabaseService db = DatabaseService.getInstance();
+    db = DatabaseService.getInstance();
+    db = DatabaseService.getInstance();
 
-    piyush.addFriend(jhon);
-    piyush.addFriend(jane);
-
-    try {
-      System.out.println(piyush.getRollNo(jane));
-    } catch (Exception e) {
-      System.out.println(e);
-    }
+    db.getDataFromTables();
+    db.putDataInDatabase();
   }
 }
