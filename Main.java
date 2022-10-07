2bdd64766c1dd7945596d4a686f12a95650617d9
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-10-07 18:44:27 +0530

    Changes

--- Main.java
+++ Main.java
@@ -1,14 +1,4 @@
-import java.util.Arrays;
-
 public class Main {
 
-  public static void main(String args[]) {
-    int[] arr = new int[5];
-    arr[0] = 10;
-    arr[1] = 20;
-    arr[2] = 30;
-    arr[3] = 40;
-
-    System.out.println(Arrays.toString(arr));
-  }
+  public static void main(String args[]) {}
 }
